09279b164ded4642818d0f1d07a39133e9ca9c84
LeaOhyeah <leasehat@gmail.com>
2025-05-26 14:26:49 +0800

    [add] model & migration

--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Destination extends Model
 {
-    use HasFactory;
-
-    protected $table = 'destinations'; // opsional jika tabelnya tidak sesuai plural
-    protected $fillable = ['title', 'description', 'image'];
+    protected $table = 'destinations';
+    protected $guarded = ['id'];
 }
