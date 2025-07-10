0eda9ac249cec6740b51d334cb2f90e621d63e16
agungsaraswati <agungsaraswati251204@gmail.com>
2025-07-10 23:04:09 +0800

    [update] menambah link sosial media dan link navigasi footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,17 +6,21 @@
         <div class="ftco-footer-widget mb-3" style="text-align:left;">
           <h2 class="ftco-heading-2" style="font: size 2.2xl;; font-weight:700; margin-bottom:32px;">Pesona Bali</h2>
           <p class="small text-white" style="font: size 2xl;;">
-            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+            blind texts.
           </p>
           <ul class="ftco-footer-social list-unstyled mt-3">
             <li class="ftco-animate d-inline-block me-3">
-              <a href="#" class="social-icon" style="font: size 2xl;;"><span class="icon-twitter"></span></a>
+              <a href="https://twitter.com" target="_blank" class="social-icon" style="font-size: 1.5rem;"><span
+                  class="icon-twitter"></span></a>
             </li>
             <li class="ftco-animate d-inline-block me-3">
-              <a href="#" class="social-icon" style="font: size 2xl;;"><span class="icon-facebook"></span></a>
+              <a href="https://facebook.com" target="_blank" class="social-icon" style="font-size: 1.5rem;"><span
+                  class="icon-facebook"></span></a>
             </li>
             <li class="ftco-animate d-inline-block">
-              <a href="#" class="social-icon" style="font: size 2xl;;"><span class="icon-instagram"></span></a>
+              <a href="https://instagram.com" target="_blank" class="social-icon" style="font-size: 1.5rem;"><span
+                  class="icon-instagram"></span></a>
             </li>
           </ul>
         </div>
@@ -27,10 +31,10 @@
         <div class="ftco-footer-widget mb-3" style="text-align:left;">
           <h2 class="ftco-heading-2" style="font: size 2.2xl;; font-weight:700; margin-bottom:32px;">Information</h2>
           <ul class="list-unstyled small">
-            <li><a href="#" class="py-1 d-block text-white" style="font: size 2xl;;">Home</a></li>
-            <li><a href="#" class="py-1 d-block text-white" style="font: size 2xl;;">About Us</a></li>
-            <li><a href="#" class="py-1 d-block text-white" style="font: size 2xl;;">Destination</a></li>
-            <li><a href="#" class="py-1 d-block text-white" style="font: size 2xl;;">Contact Us</a></li>
+            <li><a href="/" class="py-1 d-block text-white" style="font-size: 2xl;">Home</a></li>
+            <li><a href="/about" class="py-1 d-block text-white" style="font-size: 2xl;">About Us</a></li>
+            <li><a href="/destinations" class="py-1 d-block text-white" style="font-size: 2xl;">Destination</a></li>
+            <li><a href="/contact" class="py-1 d-block text-white" style="font-size: 2xl;">Contact Us</a></li>
           </ul>
         </div>
       </div>
@@ -38,10 +42,13 @@
       <!-- Kolom 3 -->
       <div class="col-md-3">
         <div class="ftco-footer-widget mb-3" style="text-align:left;">
-          <h2 class="ftco-heading-2" style="font: size 2.2xl;; font-weight:700; margin-bottom:32px;">Have a Questions?</h2>
+          <h2 class="ftco-heading-2" style="font: size 2.2xl;; font-weight:700; margin-bottom:32px;">Have a Questions?
+          </h2>
           <ul class="list-unstyled small">
-            <li class="mb-2" style="font: size 2xl;;"><span class="icon icon-map-marker me-2"></span> Nangka street, Denpasar, Bali</li>
-            <li class="mb-2" style="font: size 2xl;;"><span class="icon icon-phone me-2"></span> +62 812 345 678 910</li>
+            <li class="mb-2" style="font: size 2xl;;"><span class="icon icon-map-marker me-2"></span> Nangka street,
+              Denpasar, Bali</li>
+            <li class="mb-2" style="font: size 2xl;;"><span class="icon icon-phone me-2"></span> +62 812 345 678 910
+            </li>
             <li style="font: size 2xl;;"><span class="icon icon-envelope me-2"></span> pesonabali@gmail.com</li>
           </ul>
         </div>
@@ -50,8 +57,9 @@
 
     <div class="row pt-3 mt-3">
       <div class="col-md-12 text-center small">
-         <p class="mb-0" style="font: size 2xl;;">This project is made with <i class="icon-heart text-danger"></i> by 
-          <a href="https://JelajahNusantara.com" target="_blank" class="text-white" style="font: size 2xl;;">Pesona Bali</a>
+        <p class="mb-0" style="font: size 2xl;;">This project is made with <i class="icon-heart text-danger"></i> by
+          <a href="https://JelajahNusantara.com" target="_blank" class="text-white" style="font: size 2xl;;">Pesona
+            Bali</a>
         </p>
       </div>
     </div>
